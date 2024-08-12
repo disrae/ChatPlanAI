761ec3674a105233ef3d29594a09280173df9822
Daniel Israel <danny.israel@gmail.com>
2024-08-11 17:29:10 -0700

    Initial commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import "../global.css";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 export default function Layout() {
-  return <Slot />;
+  return <Stack>
+    <Stack.Screen name="(tabs)" />
+  </Stack>;
 }
