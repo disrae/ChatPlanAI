7d1436885fa731470cef9429b2d9fc97b1c8a5bf
Daniel Israel <danny.israel@gmail.com>
2024-08-11 21:03:12 -0700

    Tab Router

--- src/app/(tabs)/summary.tsx
+++ src/app/(tabs)/summary.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
 
-export default function Chat() {
+export default function Summary() {
     return (
         <View className='justify-center items-center flex-1'>
-            <Text className='text-3xl font-medium'>Chat Dashboard</Text>
+            <Text className='text-3xl font-medium'>Summary Dashboard</Text>
         </View>
     );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,6 +3,6 @@ import { Slot, Stack } from "expo-router";
 
 export default function Layout() {
   return <Stack>
-    <Stack.Screen name="(tabs)" />
+    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
   </Stack>;
 }
